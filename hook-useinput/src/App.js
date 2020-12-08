cd909fc8414ae47836e1819a259b99ef7757ca69
yesimnoh <yesimnoh@gmail.com>
2020-12-08 11:39:54 +0900

    update useInput & index.html

--- hook-useinput/src/App.js
+++ hook-useinput/src/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
-  return { value };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>useInput</h1>
-      <input placeholder="Name" value={name.value} />
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
